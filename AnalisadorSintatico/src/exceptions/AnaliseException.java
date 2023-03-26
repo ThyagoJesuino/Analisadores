f7b2ff0f9207120242e56b858bccab3455dc3820
Thyago Jesuino da Silva <82784831+ThyagoJesuino@users.noreply.github.com>
2023-03-26 16:47:04 -0300

    Analisadores léxico e sintático.

---
+++ AnalisadorSintatico/src/exceptions/AnaliseException.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+public class AnaliseException extends Exception{
+
+	public AnaliseException(){
+		System.out.println("Analise Sintática incorreta, favor  verificar a entrada! ");
+	}
+}
---
+++ AnalisadorSintatico/src/main/Principal.java
@@ -0,0 +1,39 @@
+package main;
+
+import java.util.LinkedList;
+import analisadorLexico.AnalisadorLexico;
+import analisadorLexico.Token;
+import analisadorSintatico.AnalisadorSintatico;
+import exceptions.AnaliseException;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		LinkedList<Token> tokens = null;
+		try {
+
+			tokens = AnalisadorLexico.parceLexico("C:/Users/braba/OneDrive/Área de Trabalho/AnalisadorSintaticoD-master/AnalisadorSintaticoD-master/src/textfile.txt");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("An�lise Lexica: ");
+		 for (Token token : tokens) {
+		 System.out.println(token);
+		 }
+
+		try {
+			System.out.println();
+			System.out.printf("Analise Sintatica: ");
+			AnalisadorSintatico as = new AnalisadorSintatico(tokens);
+			as.prog();
+			System.out.println();
+			as.result();
+		} catch (Exception e) {
+			AnaliseException exception = new AnaliseException();
+		}
+
+	}
+
+}
