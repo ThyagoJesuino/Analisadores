0420fba1ec2043aea6de3dbc633a6af84fdd93c4
Thyago Jesuino da Silva <82784831+ThyagoJesuino@users.noreply.github.com>
2023-05-24 14:12:04 -0300

    Delete Analisadores-main directory

--- Analisadores-main/Analisadores-main/AnalisadorSintatico/src/exceptions/AnaliseException.java
+++
@@ -1,8 +0,0 @@
-package exceptions;
-
-public class AnaliseException extends Exception{
-
-	public AnaliseException(){
-		System.out.println("Analise Sintatica incorreta, favor  verificar a entrada! ");
-	}
-}
