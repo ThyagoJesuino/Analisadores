212586fc726192f3ba99cef7337f7c39a0f82dda
Thyago Jesuino da Silva <82784831+ThyagoJesuino@users.noreply.github.com>
2023-05-24 14:12:28 -0300

    Add files via upload

---
+++ Analisadores-main/Analisadores-main/AnalisadorSintatico/src/analisadorLexico/AnalisadorLexico.java
@@ -0,0 +1,142 @@
+package analisadorLexico;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+
+import exceptions.LexicalParseException;
+
+public class AnalisadorLexico {
+
+	public final static char LINE_TOKEN = '�';
+
+	static String RESERVED_WORD[] = { "begin", "if" ,"end", "integer", "{integer}", "real",  "{real}", "program", "var", "array", "it", "then", "of",
+			"repeat", "until", "read", "write", "do", "not", "or", "to", "div", "mod", "end", "$", "procedure", "function", "for", "pos"};
+	static String[] SYMBOLS = { "(", ")", "{", "}", "'", ";", "=", ":", ",", "." };
+	static String[] OPERATORS = { "+", "-", "*", "/", "//"};
+	static String[] OPERATORS_COMP = { ">=", "<=", "&&", "||", "==" };
+	static String[] OPERATORS_CONJ = { "U", "∩", "⊂", "⊆" };
+	static String[] ATRIBUITION = {":="};
+	static String[] LISTCMD={"if", "it", "read", "write", "do", "not", "or", "div", "mod", "for", "pos"};
+	
+	public static LinkedList<Token> parceLexico(String file) throws Exception {
+		LinkedList<Token> tokenlist = new LinkedList<>();
+		StringBuffer string = bufferFile(file);
+
+		int lineCount = 1;
+
+		String buffer = "";
+		String lineBuffer = "";
+		for (int i = 0; i < string.length(); i++) {
+			char c = string.charAt(i);
+
+			if (Character.valueOf(c).equals(LINE_TOKEN)) {
+				lineCount++;
+				lineBuffer = "";
+				continue;
+			}
+
+			if (Character.isLetter(c)) {
+				while (Character.isLetter(c)) {
+					buffer += c;
+					lineBuffer += c;
+					i++;
+					c = string.charAt(i);
+				}
+				i--;
+				tokenlist.add(new Token(lineCount,
+						contains(buffer, RESERVED_WORD) ? Token.RESERVED_WORD_TOKEN : Token.IDENTIFIER_TOKEN, buffer));
+				buffer = "";
+				continue;
+			}
+
+			if (Character.isDigit(c)) {
+				while (Character.isDigit(c)) {
+					buffer += c;
+					lineBuffer += c;
+					i++;
+					c = string.charAt(i);
+				}
+				i--;
+				tokenlist.add(new Token(lineCount, Token.INTEGER_TOKEN, buffer));
+				buffer = "";
+				continue;
+			}
+			if (i + 1 < string.length()
+					&& contains(String.valueOf(c) + String.valueOf(string.charAt(i + 1)), OPERATORS_COMP)) {
+				tokenlist.add(new Token(lineCount, Token.OPERATOR_TOKEN_COMP,
+						String.valueOf(c) + String.valueOf(string.charAt(i + 1))));
+				lineBuffer += c;
+				i++;
+				continue;
+			}
+			if (contains(String.valueOf(c), OPERATORS_CONJ)) {
+				tokenlist.add(new Token(lineCount, Token.OPERATOR_TOKEN_CONJ, String.valueOf(c)));
+				lineBuffer += c;
+				continue;
+			}
+			if (contains(String.valueOf(c), OPERATORS)) {
+				tokenlist.add(new Token(lineCount, Token.OPERATOR_TOKEN, String.valueOf(c)));
+				lineBuffer += c;
+				continue;
+			}
+			if (contains(String.valueOf(c) + String.valueOf(string.charAt(i + 1)), ATRIBUITION)) {
+				tokenlist.add(new Token(lineCount, Token.ATRIBUITION,
+				 		String.valueOf(c) + String.valueOf(string.charAt(i + 1))));
+				lineBuffer += c;
+				i++;
+				continue;
+			}
+		
+			if (contains(String.valueOf(c), SYMBOLS)) {
+				tokenlist.add(new Token(lineCount, Token.SYMBOL_TOKEN, String.valueOf(c)));
+				lineBuffer += c;
+				continue;
+			}
+
+			if (c != ' ' && (int) c > 60) {
+				throw new LexicalParseException(lineCount, lineBuffer, c);
+			}
+			lineBuffer += c;
+		}
+
+		return tokenlist;
+	}
+
+	public static boolean contains(String s, String v[]) {
+		for (int i = 0; i < v.length; i++) {
+			if (v[i].equals(s))
+				return true;
+		}
+		return false;
+	}
+
+	public static StringBuffer bufferFile(String file) {
+		StringBuffer string = null;
+		try {
+			string = new StringBuffer();
+			FileReader fileReader = new FileReader(file);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			String line = bufferedReader.readLine();
+			if (line != null) {
+				string.append(line);
+				string.append(LINE_TOKEN);
+			}
+			while (line != null) {
+				line = bufferedReader.readLine();
+				if (line != null) {
+					string.append(line);
+					string.append(LINE_TOKEN);
+				}
+
+			}
+			fileReader.close();
+		} catch (IOException e) {
+			System.err.printf("[Err0] Open File: %s.\n", e.getMessage());
+			return null;
+		}
+		return string;
+	}
+
+}
